5cc2fe375740ebd750ae57ce44781077a58fc9e1
unknown <moulisaha123@gmail.com>
2024-01-02 10:43:47 +0530

    Updated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
+    const expenseDate = new Date(2023,12,1);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 300;
+
     return(
-        <div>
-            <div>Date</div>
-            <div>
-                <h2>Title</h2>
-                <div>Amount</div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">Rs{expenseAmount}</div>
             </div>
         </div>
     );
