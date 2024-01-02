a414ed68e7853adde5b52373ef6dc0aaf987d36d
unknown <moulisaha123@gmail.com>
2024-01-02 10:14:30 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HIEEEEEEEEEE....</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
